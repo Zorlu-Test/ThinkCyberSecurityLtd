a4e84ddb3c4f45a844e2cf598100d691d380614e
Ensar <zorlu.qa.test@gmail.com>
2023-03-01 18:53:32 +0000

    commit

--- src/test/java/com/ThinkCyberSecurity/step_Definition/Factorial_Steps.java
+++ src/test/java/com/ThinkCyberSecurity/step_Definition/Factorial_Steps.java
@@ -15,64 +15,6 @@ public class Factorial_Steps extends Pages {
 
     static long inputUser;
 
-//    @Test
-//    public void test1() {
-//
-//
-//
-//        getDriver().get("https://qainterview.pythonanywhere.com/");
-//
-//        WebElement enterInt= getDriver().findElement(By.xpath("//input[@id='number']"));
-//        WebElement calculate= getDriver().findElement(By.xpath("//button[@type='submit']"));
-//
-//
-//        enterInt.click();
-//        enterInt.sendKeys("2");
-//        calculate.click();
-//
-//
-//        String formColor = enterInt.getAttribute("style");
-//
-//
-//        Assertions.assertTrue(!formColor.toLowerCase().contains("red"),"Entered value refused");
-//
-//
-//        // Get logs
-//        LogEntries logs = getDriver().manage().logs().get(LogType.BROWSER);
-//
-//        // Iterate over the log entries and print them to the console
-//        for (LogEntry entry : logs) {
-//            System.out.println(entry.getLevel());
-//            System.out.println(entry.getMessage());
-//        }
-//
-//
-//        try {
-//            // Create a new URL object
-//            URL url = new URL("https://qainterview.pythonanywhere.com/");
-//
-//            // Open an HTTP connection to the URL
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//
-//            // Send an HTTP GET request
-//            connection.setRequestMethod("GET");
-//
-//            // Get the response headers
-//            Map<String, List<String>> headers = connection.getHeaderFields();
-//
-//            // Print the headers
-//            for (String key : headers.keySet()) {
-//                System.out.println(key + ": " + headers.get(key));
-//            }
-//
-//            // Disconnect the HTTP connection
-//            connection.disconnect();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
     @Given("I am on the website")
     public void iAmOnTheWebsite() {
         Driver.getDriver().get(ConfigurationReader.get("baseUrl"));
@@ -126,7 +68,6 @@ public class Factorial_Steps extends Pages {
     public void verifyTheNumberCalculated() {
         methods().assertResultDisplayed();
     }
-
 
 
 }
